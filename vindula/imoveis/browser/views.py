5d019472c42741d2eff23a84578df10c29d3454c
Jefferson Ramal de Almeida <jeffersonramal@liberiun.com>
2012-06-27 14:59:10 -0300

    Removendo somente imóveis que não estão no WS

--- vindula/imoveis/browser/views.py
+++ vindula/imoveis/browser/views.py
@@ -1,9 +1,11 @@
+# -*- coding:utf-8 -*-
 from five import grok
 from plone.directives import dexterity, form
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from suds.client import Client
+from datetime import *
 
 from vindula.mongodbconnector.interfaces import IMongoDBConnector
 from vindula.imoveis.content.imovel import IImovel
@@ -54,25 +56,38 @@ class WSIntegrationView(grok.View):
             print 'Importing Data form Ws: ', vars['ws_address']
             try:
                 lista_alugueis = client.service.listarAluguel(True)
+                lista_ids_mongo = [i.Id for i in imoveis_folder.db.Imovel.find({'Aluguel':True})]
+                lista_ids_ws = []
             except:
                 client.service.logar(vars['ws_user'],vars['ws_password'])
                 lista_alugueis = client.service.listarAluguel(True)
             for imovel in lista_alugueis:
+                lista_ids_ws.append(imovel.Id)
                 imovel_obj = imoveis_folder.get(imovel.Id,params_obj=imovel)
                 imovel_obj.Aluguel = True
                 imovel_obj.save()
+            
+            remove_lista_ids_mongo = list(set(lista_ids_mongo).difference(set(lista_ids_ws)))
+            for i in remove_lista_ids_mongo:
+                imoveis_folder.db.Imovel.remove({'Id':i})
+                print 'Imóvel', i, 'removido'
+                
+            print datetime.now().strftime('%d/%m/%Y - %H:%M'), ' - Imóveis atualizados'
                 
         
         if vars['ws_integration'] == True:
             print 'Importing Data form Ws: ', vars['ws_address']
             try:
                 lista_vendas = client.service.listarVendaRede(True)
+                lista_ids_mongo_vendas = [i.Id for i in imoveis_folder.db.Imovel.find({'Venda':True})]
+                lista_ids_ws = []
             except:
                 try:
                     client.service.logar(vars['ws_user'],vars['ws_password'])
                     lista_vendas = client.service.listarVendaRede(True)
                 except:
                     print 'Metodo do wimoveis fora do ar, adiando importacao.'
+                    print datetime.now().strftime('%d/%m/%Y - %H:%M'), ' - Imóveis não atualizados'
                     return
                 
             for imovel in lista_vendas:
@@ -92,7 +107,8 @@ class WSIntegrationView(grok.View):
                             client.service.logar(vars['ws_user'],vars['ws_password'])
                             imovel = client.service.pegarImovel(id)
                         except:
-                            print 'Metodo do wimoveis fora do ar, adiando importacao.'
+                            print 'Métodos do wimoveis fora do ar, adiando importação.'
+                            print datetime.now().strftime('%d/%m/%Y - %H:%M'), ' - Imóveis não atualizados'
                             return
                     try:
                         imovel_obj = imoveis_folder.get(imovel.Id,params_obj=imovel)
@@ -102,11 +118,17 @@ class WSIntegrationView(grok.View):
                         
                     imovel_obj.Venda = True
                     imovel_obj.save()
+            remove_lista_ids_mongo_vendas = list(set(lista_ids_mongo_vendas).difference(set(lista_ids_ws)))
+            for i in remove_lista_ids_mongo_vendas:
+                imoveis_folder.db.Imovel.remove({'Id':i})
+                print 'Imóvel', i, 'removido'
+                    
+                print datetime.now().strftime('%d/%m/%Y - %H:%M'), ' - Foto Imóvel atualizado'
                 
         
         for imovel in imoveis_folder.collection.find():
             print 'Importing Photos Imovel: ', imovel.Id
-	    #O metodo listarFoto recebe o id do imovel e uma nova flag,sendo 1(fotos dos imoveis ativos) 0(fotos dos imoveis inativos)
+        #O metodo listarFoto recebe o id do imovel e uma nova flag,sendo 1(fotos dos imoveis ativos) 0(fotos dos imoveis inativos)
             if imovel.Aluguel == True:
                 try:
                     fotos = client.service.listarFoto(imovel.Id,1)
@@ -116,6 +138,7 @@ class WSIntegrationView(grok.View):
                         fotos = client.service.listarFoto(imovel.Id,1)
                     except:
                         print 'Metodo do wimoveis fora do ar, adiando importacao.'
+                        print datetime.now().strftime('%d/%m/%Y - %H:%M'), ' - Foto dos Imóveis não atualizados'
                         return
                 for foto in fotos:
                     foto_obj = fotos_folder.get(foto.Id,params_obj=foto)
@@ -124,4 +147,3 @@ class WSIntegrationView(grok.View):
             
         print 'Imoveis na base: ', imoveis_folder.collection.find().count()
         return ''        
-
